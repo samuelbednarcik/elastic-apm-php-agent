7b7d08f4381795bcbd61561f827e2d61480f11a7
Samuel Bednarcik <samuel.bednarcik@ringier.sk>
2018-11-16 12:50:11 +0100

    create transaction event

---
+++ src/Events/Transaction.php
@@ -0,0 +1,240 @@
+<?php
+
+namespace SamuelBednarcik\ElasticAPMAgent\Events;
+
+class Transaction
+{
+    const TYPE_REQUEST = 'request';
+    const TYPE_BACKGROUND_JOB = 'backgroundjob';
+
+    /**
+     * @var array|null
+     */
+    protected $context;
+
+    /**
+     * @var int|null
+     */
+    protected $duration;
+
+    /**
+     * @var string|null
+     */
+    protected $name;
+
+    /**
+     * @var string|null
+     */
+    protected $result;
+
+    /**
+     * @var string|null
+     */
+    protected $type;
+
+    /**
+     * @var bool|null
+     */
+    protected $sampled;
+
+    /**
+     * @var string|null
+     */
+    protected $id;
+
+    /**
+     * @var string|null
+     */
+    protected $traceId;
+
+    /**
+     * @var string|null
+     */
+    protected $parentId;
+
+    /**
+     * @var array|null
+     */
+    protected $spanCount;
+
+    /**
+     * @var array|null
+     */
+    protected $marks;
+
+    /**
+     * @return array|null
+     */
+    public function getContext(): ?array
+    {
+        return $this->context;
+    }
+
+    /**
+     * @param array|null $context
+     */
+    public function setContext(?array $context): void
+    {
+        $this->context = $context;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    /**
+     * @param int|null $duration
+     */
+    public function setDuration(?int $duration): void
+    {
+        $this->duration = $duration;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param null|string $name
+     */
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getResult(): ?string
+    {
+        return $this->result;
+    }
+
+    /**
+     * @param null|string $result
+     */
+    public function setResult(?string $result): void
+    {
+        $this->result = $result;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param null|string $type
+     */
+    public function setType(?string $type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function getSampled(): ?bool
+    {
+        return $this->sampled;
+    }
+
+    /**
+     * @param bool|null $sampled
+     */
+    public function setSampled(?bool $sampled): void
+    {
+        $this->sampled = $sampled;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param null|string $id
+     */
+    public function setId(?string $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getTraceId(): ?string
+    {
+        return $this->traceId;
+    }
+
+    /**
+     * @param null|string $traceId
+     */
+    public function setTraceId(?string $traceId): void
+    {
+        $this->traceId = $traceId;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getParentId(): ?string
+    {
+        return $this->parentId;
+    }
+
+    /**
+     * @param null|string $parentId
+     */
+    public function setParentId(?string $parentId): void
+    {
+        $this->parentId = $parentId;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getSpanCount(): ?array
+    {
+        return $this->spanCount;
+    }
+
+    /**
+     * @param array|null $spanCount
+     */
+    public function setSpanCount(?array $spanCount): void
+    {
+        $this->spanCount = $spanCount;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getMarks(): ?array
+    {
+        return $this->marks;
+    }
+
+    /**
+     * @param array|null $marks
+     */
+    public function setMarks(?array $marks): void
+    {
+        $this->marks = $marks;
+    }
+}
