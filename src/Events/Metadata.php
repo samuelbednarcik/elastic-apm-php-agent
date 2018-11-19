f65ceb3bcfd3f9f70a473b65d8c0ae08869ee76f
Samuel Bednarcik <samuel.bednarcik@ringier.sk>
2018-11-19 14:44:45 +0100

    add getters and setters to metadata entity

--- src/Events/Metadata.php
+++ src/Events/Metadata.php
@@ -23,4 +23,68 @@ class Metadata
      * @var array|null
      */
     protected $user;
+
+    /**
+     * @return array|null
+     */
+    public function getService(): ?array
+    {
+        return $this->service;
+    }
+
+    /**
+     * @param array|null $service
+     */
+    public function setService(?array $service): void
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getProcess(): ?array
+    {
+        return $this->process;
+    }
+
+    /**
+     * @param array|null $process
+     */
+    public function setProcess(?array $process): void
+    {
+        $this->process = $process;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getSystem(): ?array
+    {
+        return $this->system;
+    }
+
+    /**
+     * @param array|null $system
+     */
+    public function setSystem(?array $system): void
+    {
+        $this->system = $system;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getUser(): ?array
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param array|null $user
+     */
+    public function setUser(?array $user): void
+    {
+        $this->user = $user;
+    }
 }
