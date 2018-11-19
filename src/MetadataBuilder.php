f94a618c093defd8712e1ce881f7ba3729f75aa6
Samuel Bednarcik <samuel.bednarcik@ringier.sk>
2018-11-19 14:00:02 +0100

    create metadata builder class

---
+++ src/MetadataBuilder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SamuelBednarcik\ElasticAPMAgent;
+
+class MetadataBuilder extends AbstractEventBuilder
+{
+    /**
+     * @param string $name
+     * @param string|null $version
+     * @param string|null $environment
+     * @param string|null $framework
+     * @return array
+     */
+    public static function buildService(
+        string $name,
+        string $version = null,
+        string $environment = null,
+        string $framework = null
+    ): array {
+        return [
+            'agent' => [
+                'name' => Agent::AGENT_NAME,
+                'version' => Agent::AGENT_VERSION
+            ],
+            'framework' => $framework,
+            'language' => [
+                'name' => 'PHP',
+                'version' => phpversion()
+            ],
+            'name' => $name,
+            'environment' => $environment,
+            'version' => $version,
+        ];
+    }
+}
